cc0ee3f4aeb43a3c91787645334e1c59abdd4285
SimonQida <1784297375@qq.com>
2018-03-17 07:54:28 +0800

    basic funciton done

--- doc_in/src/newlei.java
+++ doc_in/src/newlei.java
@@ -1,27 +1,93 @@
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+class doc {
+	String name;
+	String text;
+	int entercount =1,wordcount = 0,symbolcount=0;
+	boolean[] temp ={};
+	public doc(){}
+	public doc(String n,String t,boolean[] para){
+		name = n;
+		text = t;
+		temp = para;
+		cal();
+	}
+	public String  mes(){
+		String message= "name:"+name+"\n";
+		if(temp[0]==true)
+			message +="\tentercount:"+entercount;
+		if(temp[1] == true)
+			message +="\twordcount:"+wordcount;
+		if(temp[2] ==true)
+			message +="\tsymbolcount:"+symbolcount+"\n";
+		return message;
+	}
+	public static boolean isChinese(char c) {  
+	    return c >= 0x4E00 &&  c <= 0x9FA5;// 根据字节码判断  
+	}  
+	public static boolean isword(char c) {  
+	    return (c>='A'&&c<='Z')||(c>='a'&&c<='z')||(c>='0'&&c<='9');  
+	}  
+	public static boolean isnum(char c) {  
+	    return (c>='0'&&c<='9');  
+	}
+	public static boolean issymbol(char c){
+		return !(isChinese(c)||isword(c)||(c=='\n'));
+	}
+	private void cal(){
+		boolean isaword = false;
+			//多少行是指的多少回车吗
+			//单词数 ，如果是汉语呢，那应该是一个字是一个词咯？
+		for(int i =0;i<text.length();i++){
+			char temp = text.charAt(i);
+			if(temp=='\n')
+				entercount +=1;
+			if(isword(temp)){
+				if(!isnum(temp)){
+					isaword = true;
+				}
+			}
+			if(isChinese(temp)){
+				wordcount+=1;
+				if(isaword)
+					wordcount +=1;
+				isaword = false;
+			}
+			else if(issymbol(temp))
+			{
+				if(isaword)
+					wordcount +=1;
+				isaword = false;
+				if(issymbol(temp)){
+					symbolcount +=1;
+				}
+			}
+				
+		}
+		if(isaword) wordcount +=1;
+	}
+
+}
+class outdoc {
+	String name;
+	ArrayList children =new ArrayList();
+	public void clear(){
+		children.clear();
+	}
+}
 
 public class newlei {
-
 	/**
 	 * @param args
 	 */
-	public static boolean isChinese(char c) {  
-        return c >= 0x4E00 &&  c <= 0x9FA5;// 根据字节码判断  
-    }  
-	public static boolean isword(char c) {  
-        return (c>='A'&&c<='Z')||(c>='a'&&c<='z')||(c>='0'&&c<='9');  
-    }  
-	public static boolean isnum(char c) {  
-        return (c>='0'&&c<='9');  
-    }
-	public static boolean issymbol(char c){
-		return !(isChinese(c)||isword(c)||(c=='\n')||(c ==' '));
-	}
+	final char[] para= {'c','w','l'}; 
 	public static boolean createFile(String filePath){
 		boolean result = false;
 		File file = new File(filePath);
@@ -59,59 +125,105 @@ public class newlei {
 		}
 		return content.toString();
 	}
-	
-	public static void main(String[] args) {
+	public static void writeFileByChars(File file,String mes) throws IOException{
+		BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file)); 
+		bufferedWriter.write(mes);
+		bufferedWriter.flush();// 清空缓冲区  
+        bufferedWriter.close();// 关闭输出流  
+	}
+	public static void main(String[] args)throws Exception {
 		// TODO Auto-generated method stub
-		
-		//createFile("fucknigs");
-		boolean isaword = false;
-		String stream = readFileByChars("javaforcednote");
-		int entercount = 1,wordcount  = 0,symbolcount =0;		//多少行是指的多少回车吗
-									//单词数 ，如果是汉语呢，那应该是一个字是一个词咯？
-		for(int i =0;i<stream.length();i++){
-			char temp = stream.charAt(i);
-			if(temp=='\n')
-				entercount +=1;
-			if(isword(temp)){
-				if(!isnum(temp)){
-					isaword = true;
+		boolean  isout = false;
+		boolean[] para= {true,true,true};
+		ArrayList parachar = new ArrayList();
+		ArrayList<doc> docarr = new ArrayList<doc>();
+		outdoc outdocarr = new outdoc();
+		for(int i =0;i<args.length;i++){
+			if(args[i].charAt(0)=='-'){
+				if(args[i].length()!=2)
+					throw new Exception("a para after a -");
+				if(args[i].charAt(1)=='o'){	//参数是-o
+					isout= true;
+					if(i-1<0)				//first param can't be -o
+						throw new Exception("-o as the first para");
+					if(i+1>=args.length)
+						throw new Exception("-o as the last para");
+					if(args[i+1].charAt(0)=='-'||args[i-1].charAt(0)=='-')//the para before 
+															// or after must be filename
+						throw new Exception("no filename after||before -o");
+					if(docarr.size()==0)				//has to have at least one filename message
+						throw new Exception("no file message to write!");
+					outdoc tempo  =new outdoc();
+					for(int index =0;index<docarr.size();index++){	//
+						tempo.children.add(docarr.get(index));
+					}
+					docarr.clear();
+					outdocarr = tempo;
+				}
+				else{						//参数不是-o
+					parachar.add(args[i].charAt(1));
 				}
 			}
-			if(isChinese(temp)){
-				wordcount+=1;
-				if(isaword)
-					wordcount +=1;
-				isaword = false;
-			}
-			else if(issymbol(temp)||(temp ==' '))
-			{
-				if(isaword)
-					wordcount +=1;
-				isaword = false;
-				if(issymbol(temp)){
-					symbolcount +=1;
-				}
-			}
-				
-		}
-		if(isaword) wordcount +=1;
+			else{
+				if(isout){
+					outdocarr.name = args[i];		//类型转换吗
+					isout = false;	
+					File file = new File(args[i]);
+					if(!file.exists()){
+						createFile(args[i])	;			//unsure
+					}
+					String s="";
+					for(int index =0;index<outdocarr.children.size();index++)
+						s += ((doc) (outdocarr.children.get(index))).mes();
+					writeFileByChars(file,s);										//write into the file
 
-		System.out.println(stream);
-		System.out.println("entercount: ");
-		System.out.println(entercount);
-		System.out.println("wordcount: ");
-		System.out.println(wordcount);
-		System.out.println("symbolcount: ");
-		System.out.println(symbolcount);
-		/*char c = 'a';
-		boolean allzimu = true;
-		while(c<='Z'){
-			if(isChinese(c)){
-				allzimu = false;
-				break;
+					//code outputfile code here
+					
+					
+					outdocarr.clear();
 				}
+				else{
+					if(parachar.size() ==0){
+						doc tempd = new doc(args[i],readFileByChars(args[i]),para);
+						System.out.println(tempd.mes());	
+					}
+					else{
+						for(int index=0;index<parachar.size();index++){
+						boolean[] x = getpara(parachar);
+						parachar.clear();
+						doc tempd= new doc(args[i],readFileByChars(args[i]),x);	//set an obj and clear the parachar
+						docarr.add(tempd);
+						System.out.println(tempd.mes());										//print the result
+						
+					}
+				}
+			}	
 		}
-		System.out.println(allzimu);*/
+		//createFile("fucknigs");
+		//String stream = readFileByChars("javaforcednote");
+		//doc d = new doc("javaforcednote",stream,new boolean[]{true,false});
+		//System.out.println(d.mes());
 	}
 
 }
+	private static boolean[] getpara(ArrayList parachar) throws Exception{
+		// TODO Auto-generated method stub
+		boolean[] t ={false,false,false};
+		for(int index=0;index<parachar.size();index++){
+			switch ((char)(parachar.get(index))){
+			case 'c':
+				t[0] =true;
+				break;
+			case 'w':
+				t[1]=true;
+				break;
+			case 'l':
+				t[2] =true;
+				break;
+			default:
+				throw new Exception("not exist such para");
+			}
+		}
+		return t;
+		}
+	}
